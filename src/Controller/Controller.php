b7253ad2a09c326e9f10bf5fd393697e8b0cfce2
AdamDanniel <dadam4696@gmail.com>
2025-03-26 13:26:33 +0100

    contenu-ue v1

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -19,4 +19,10 @@ class Controller extends AbstractController
     {
         return $this->render('profile.html.twig');
     }
+
+    #[Route('/UE/contenu', name:'contenu_UE')]
+    public function contenu_UE() : Response
+    {
+        return $this->render('contenu_ue.html.twig');
+    }
 }
