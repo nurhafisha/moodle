4313d5f270b4f6ae01f34016f5cb7393baa57dbb
fisha <nurhafisha15@gmail.com>
2025-03-27 09:18:49 +0100

    public

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -49,7 +49,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-         return new RedirectResponse($this->urlGenerator->generate('index'));
+         return new RedirectResponse($this->urlGenerator->generate('profile'));
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
