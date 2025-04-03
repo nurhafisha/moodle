5132c12da63f54252c6729333cf2ded3c50e5b33
Akmal <akmalsabri73@gmail.com>
2025-04-03 21:22:51 +0200

    access to add UE + image

--- src/Form/UEType.php
+++ src/Form/UEType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\UE;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,10 +14,20 @@ class UEType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            // ->add('idUE')
-            ->add('codeUE')
-            ->add('nomUE')
-            ->add('imageUE')
+            ->add('codeUE', TextType::class, [
+                'label' => 'Code UE', // Custom label
+                'attr' => ['class' => 'form-control'], // Add custom classes if needed
+            ])
+            ->add('nomUE', TextType::class, [
+                'label' => 'Nom UE', // Custom label
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('imageUE', FileType::class, [
+                'label' => 'Upload Image',
+                'mapped' => false, // Prevent automatic mapping to the entity
+                'required' => false,
+                'attr' => ['class' => 'form-control'],
+            ])
         ;
     }
 
