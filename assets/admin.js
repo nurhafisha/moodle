4c45a0314ecedb8fd690ac07f55798e5c56c2d48
Akmal <akmalsabri73@gmail.com>
2025-04-03 08:02:53 +0200

    onglet-admin

---
+++ assets/admin.js
@@ -0,0 +1,9 @@
+function showOngletUser() {
+  document.getElementById("onglet-user").style.display = "block";
+  document.getElementById("onglet-ue").style.display = "none";
+}
+
+function showOngletUE() {
+  document.getElementById("onglet-user").style.display = "none";
+  document.getElementById("onglet-ue").style.display = "block";
+}
