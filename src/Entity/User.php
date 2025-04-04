8f15faccb66fc723ce2aaedd62119fed3dfc1da5
Akmal <akmalsabri73@gmail.com>
2025-04-04 12:46:16 +0200

    delete default role GUEST

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -83,7 +83,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_GUEST';
+        // $roles[] = 'ROLE_GUEST';
 
         return array_unique($roles);
         // return $this->roles;
