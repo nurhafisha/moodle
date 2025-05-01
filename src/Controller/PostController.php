e99505eee338479df6b3903d54587e291ce8555d
Akmal <akmalsabri73@gmail.com>
2025-05-01 23:06:31 +0200

    add breadcrumb for UE page

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\UERepository;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
+// use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 #[Route('/mes-cours/{code_ue}')]
